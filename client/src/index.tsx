72c6fed2f8da3c824b1d48c827bcd76a98483853
Christopher <Chris1Bobsy@Gmail.com>
2022-05-23 18:54:49 +0100

    update index.css to index.scss

--- client/src/index.tsx
+++ client/src/index.tsx
@@ -1,7 +1,9 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import "./index.css";
+import "./index.scss";
 import App from "./App";
+
+import "./styles/_global-imports.scss";
 
 const root = ReactDOM.createRoot(
   document.getElementById("root") as HTMLElement
