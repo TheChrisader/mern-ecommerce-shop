76d6c10c84494de91b0de10b4e385a142d8616ab
Christopher <Chris1Bobsy@Gmail.com>
2022-05-26 17:24:29 +0100

    add functionality for scroll animation

--- client/src/components/Products/Products.tsx
+++ client/src/components/Products/Products.tsx
@@ -1,3 +1,4 @@
+import { useState, useRef, useEffect } from "react";
 import Product from "../Product/Product";
 
 import "./Products.scss";
@@ -11,8 +12,35 @@ export type Props = {
 };
 
 const Products: React.FC = () => {
+  const productsRef = useRef<HTMLDivElement | null>(null);
+  const [isVisible, setIsVisible] = useState<boolean>(false);
+
+  const callback = (entries: any) => {
+    const [entry] = entries;
+    console.log(entry.isIntersecting);
+    if (!isVisible) setIsVisible(entry.isIntersecting);
+  };
+
+  const options = {
+    root: null,
+    rootMargin: "0px",
+    threshold: 0,
+  };
+
+  useEffect(() => {
+    const observer = new IntersectionObserver(callback, options);
+    if (productsRef.current) observer.observe(productsRef.current);
+
+    return () => {
+      if (productsRef.current) observer.unobserve(productsRef.current);
+    };
+  }, [productsRef, options]);
+
   return (
-    <div className="products-wrapper">
+    <div
+      className={"products-wrapper " + (isVisible && "products-active")}
+      ref={productsRef}
+    >
       <Product
         img="https://static.live.templately.com/2021/06/297c6a78-image0.png"
         title="Learn Python Programming Masterclass"
