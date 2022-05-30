3c1e488b93fd7a84e50820bf4a7231bf903ba86b
Christopher <Chris1Bobsy@Gmail.com>
2022-05-30 14:51:59 +0100

    setup react-router-dom

--- client/src/components/Products/Products.tsx
+++ client/src/components/Products/Products.tsx
@@ -1,4 +1,4 @@
-import { useState, useRef, useEffect } from "react";
+import { useRef } from "react";
 import useObserver from "../../utils/hooks/useObserver";
 import Product from "../Product/Product";
 
---
+++ client/src/pages/Login/Login.tsx
@@ -0,0 +1,7 @@
+import "./Login.scss";
+
+const Login: React.FC = () => {
+  return <div>Login</div>;
+};
+
+export default Login;
