e6aa81e835faf75145457a46785e0a14d92cdf1f
Christopher <Chris1Bobsy@Gmail.com>
2022-06-06 23:10:45 +0100

    update css class names

--- client/src/components/Product/Product.tsx
+++ client/src/components/Product/Product.tsx
@@ -12,7 +12,7 @@ const Product: React.FC<Props> = ({ img, title, price, oldPrice }) => {
     <div className="product-item-wrapper">
       <img className="product-item-image " src={img} alt="" />
       <div className="product-item-text-wrapper">
-        <h3 className="product-title">{title}</h3>
+        <h3 className="product-item-title">{title}</h3>
         <div>
           <span className="product-price">${price} </span>
           {oldPrice && <s className="product-price">${oldPrice}</s>}
