576515e49a011be44dadd5ab6f3bc5695108f9a6
Christopher <Chris1Bobsy@Gmail.com>
2022-06-14 19:10:07 +0100

    create product page

--- client/src/pages/Products/ProductsPage.tsx
+++ client/src/pages/Products/ProductsPage.tsx
@@ -1,7 +1,49 @@
-import React from "react";
+import Products from "../../components/Products/Products";
 
-const ProductsPage = () => {
-  return <div>ProductsPage</div>;
+import "./ProductPage.scss";
+
+const ProductsPage: React.FC = () => {
+  return (
+    <>
+      <div className="filter-wrapper">
+        <div className="filter-products-wrapper">
+          <span className="filter-products">Filter Products:</span>
+          <div className="filter-products-options-wrapper">
+            <select name="" id="" className="filter-products-options">
+              <option value="" selected disabled>
+                Color
+              </option>
+              <option value="">White</option>
+              <option value="">Black</option>
+              <option value="">Blue</option>
+              <option value="">Red</option>
+              <option value="">Yellow</option>
+            </select>
+            <select name="" id="" className="filter-products-options">
+              <option value="">White</option>
+              <option value="">Black</option>
+              <option value="">Blue</option>
+              <option value="">Red</option>
+              <option value="">Yellow</option>
+            </select>
+          </div>
+        </div>
+        <div className="filter-products-wrapper">
+          <span className="filter-products">Sort Products:</span>
+          <div className="filter-products-options-wrapper">
+            <select name="" id="" className="filter-products-options">
+              <option value="">White</option>
+              <option value="">Black</option>
+              <option value="">Blue</option>
+              <option value="">Red</option>
+              <option value="">Yellow</option>
+            </select>
+          </div>
+        </div>
+      </div>
+      <Products />
+    </>
+  );
 };
 
 export default ProductsPage;
