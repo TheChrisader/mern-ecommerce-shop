dd4c886a4c88fe73b7e86b70d7de9484938bf6a6
Christopher <Chris1Bobsy@Gmail.com>
2022-06-19 15:09:43 +0100

    create error middleware

--- api/utils/verifyToken.js
+++ api/utils/verifyToken.js
@@ -1,14 +1,15 @@
 const jwt = require("jsonwebbtoken");
 const config = require("../config/config");
+const { createError } = require("./error");
 
 const { JWT_SEC } = config;
 
 const verifyToken = (req, res, next) => {
   const token = req.cookies.access_token;
-  if (!token) return next(console.log("You are not authenticated"));
+  if (!token) return next(createError(401, "You are not authenticated"));
 
   jwt.verify(token, JWT_SEC, (err, user) => {
-    if (err) return next(console.log("Token invalid"));
+    if (err) return next(createError(403, "Invalid Token"));
     req.user = user;
     next();
   });
@@ -19,7 +20,7 @@ const verifyUser = (req, res, next) => {
     if (req.user.id === req.params.id || req.user.isAdmin) {
       next();
     } else {
-      return next(console.log("Forbidden"));
+      return next(createError(403, "Forbidden Access"));
     }
   });
 };
@@ -29,7 +30,7 @@ const verifyAdmin = (req, res, next) => {
     if (req.user.isAdmin) {
       next();
     } else {
-      return next(console.log("Forbidden"));
+      return next(createError(403, "Forbidden Access"));
     }
   });
 };
