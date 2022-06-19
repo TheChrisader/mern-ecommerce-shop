3634c2185e0cf16b1ca8e8166791e803e25c11ee
Christopher <Chris1Bobsy@Gmail.com>
2022-06-19 21:06:35 +0100

    add middleware, routes

--- api/index.js
+++ api/index.js
@@ -1,10 +1,30 @@
 const express = require("express");
-const dotenv = require("dotenv");
+const cors = require("cors");
+const cookieParser = require("cookie-parser");
 const dbConnect = require("./config/db");
 
-dotenv.config();
+const authRoute = require("./routes/auth.routes");
 
 const app = express();
+
+//middleware
+app.use(express.json());
+app.use(cors());
+app.use(cookieParser());
+
+//routes
+app.use("/api/auth", authRoute);
+
+app.use((err, req, res, next) => {
+  const errorStatus = err.status || 500;
+  const errorMessage = err.message || "Something went wrong";
+  return res.status(errorStatus).json({
+    success: false,
+    status: errorStatus,
+    message: errorMessage,
+    stack: err.stack,
+  });
+});
 
 app.listen(8800, async () => {
   try {
