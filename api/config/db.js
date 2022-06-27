ac3eaef46a91c2d3faa27525318a316c060b4463
Christopher <Chris1Bobsy@Gmail.com>
2022-06-27 15:07:39 +0100

    add try catch block to database connect function

--- api/config/db.js
+++ api/config/db.js
@@ -4,8 +4,12 @@ const config = require("./config");
 const { MONGO_URL } = config;
 
 const connectDB = async () => {
-  await mongoose.connect(MONGO_URL);
-  console.log("DB is connected");
+  try {
+    await mongoose.connect(MONGO_URL);
+    console.log("DB is connected");
+  } catch (err) {
+    console.log("Database connection failed");
+  }
 };
 
 module.exports = connectDB;
