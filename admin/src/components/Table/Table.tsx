8aec636add8f0f5f77cd28950ac46845fccbce07
Christopher <Chris1Bobsy@Gmail.com>
2022-08-06 18:08:27 +0100

    add checkbox functionality to table component

--- admin/src/components/Table/Table.tsx
+++ admin/src/components/Table/Table.tsx
@@ -1,17 +1,45 @@
+import { useRef } from "react";
+
 import "./Table.scss";
 
 type tableProps = {
   columns: any[];
   rows: any[];
   items: any[];
+  check?: boolean;
 };
 
-const Table = ({ columns, items, rows }: tableProps) => {
+const Table = ({ columns, items, rows, check }: tableProps) => {
+  const allRef = useRef<HTMLInputElement | null>(null);
+  const refs = useRef<HTMLInputElement[]>([]);
+
   return (
     <div className="table-wrapper">
       <table className="table">
         <thead>
           <tr>
+            {check && (
+              <th>
+                <input
+                  className="table-check"
+                  type="checkbox"
+                  ref={allRef}
+                  onChange={(e) => {
+                    if (e.target.checked === false) {
+                      refs.current.forEach((ref) => {
+                        if (ref === null) return;
+                        return (ref.checked = false);
+                      });
+                    } else {
+                      refs.current.forEach((ref) => {
+                        if (ref === null) return;
+                        return (ref.checked = true);
+                      });
+                    }
+                  }}
+                />
+              </th>
+            )}
             {columns.map((column) => (
               <th className="table-column-item" key={column.id}>
                 {column.name}
@@ -20,8 +48,22 @@ const Table = ({ columns, items, rows }: tableProps) => {
           </tr>
         </thead>
         <tbody>
-          {items.map((item) => (
+          {items.map((item, i) => (
             <tr key={item.id} className="table-row">
+              {check && (
+                <td>
+                  <input
+                    className="table-check"
+                    ref={(element) => {
+                      if (element) refs.current[i] = element;
+                    }}
+                    type="checkbox"
+                    onChange={(e) => {
+                      if (allRef.current) allRef.current.checked = false;
+                    }}
+                  />
+                </td>
+              )}
               {rows.map((row) => (
                 <td className="table-row-item">{row.name(item)}</td>
               ))}
