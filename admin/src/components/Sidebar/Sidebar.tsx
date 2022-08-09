34ba2813e5b40a53cccf78b6f698cae7a68bb5b8
Christopher <Chris1Bobsy@Gmail.com>
2022-08-09 15:43:41 +0100

    add sidebar links

--- admin/src/components/Sidebar/Sidebar.tsx
+++ admin/src/components/Sidebar/Sidebar.tsx
@@ -10,19 +10,19 @@ const Sidebar = () => {
           <h2 className="sidebar-title">QUICK MENU</h2>
           <nav className="sidebar-nav">
             <ul className="sidebar-item">
-              <Link to="/" className="link">
+              <Link to="/" className="link sidebar-link">
                 <i className="fa-solid fa-house sidebar-icon"></i>
-                <span className="sidebar-item-name">Dashboard</span>
+                <span className="sidebar-item-name">Home</span>
               </Link>
             </ul>
             <ul className="sidebar-item">
-              <Link to="/products" className="link">
+              <Link to="/products" className="link sidebar-link">
                 <i className="fa-solid fa-bag-shopping sidebar-icon"></i>
                 <span className="sidebar-item-name">Products</span>
               </Link>
             </ul>
             <ul className="sidebar-item">
-              <Link to="/users" className="link">
+              <Link to="/users" className="link sidebar-link">
                 <i className="fa-solid fa-users sidebar-icon"></i>
                 <span className="sidebar-item-name">Users</span>
               </Link>
@@ -33,13 +33,13 @@ const Sidebar = () => {
           <h2 className="sidebar-title">CREATE/EDIT</h2>
           <nav className="sidebar-nav">
             <ul className="sidebar-item">
-              <Link to="/product/:slug" className="link">
+              <Link to="/product/:slug" className="link sidebar-link">
                 <i className="fa-solid fa-circle-plus sidebar-icon"></i>
                 <span className="sidebar-item-name">Product</span>
               </Link>
             </ul>
             <ul className="sidebar-item">
-              <Link to="/user/:id" className="link">
+              <Link to="/user/:id" className="link sidebar-link">
                 <i className="fa-solid fa-circle-plus sidebar-icon"></i>
                 <span className="sidebar-item-name">User</span>
               </Link>
@@ -50,13 +50,13 @@ const Sidebar = () => {
           <h2 className="sidebar-title">ACCOUNT</h2>
           <nav className="sidebar-nav">
             <ul className="sidebar-item">
-              <Link to="/profile" className="link">
+              <Link to="/profile" className="link sidebar-link">
                 <i className="fa-solid fa-id-card sidebar-icon"></i>
                 <span className="sidebar-item-name">Profile</span>
               </Link>
             </ul>
             <ul className="sidebar-item">
-              <Link to="/settings" className="link">
+              <Link to="/settings" className="link sidebar-link">
                 <i className="fa-solid fa-gears sidebar-icon"></i>
                 <span className="sidebar-item-name">Settings</span>
               </Link>
