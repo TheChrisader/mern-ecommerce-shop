7d7a62c86e31bf0cdcfc3a5657f6470f4eb2bde2
Christopher <Chris1Bobsy@Gmail.com>
2022-08-25 00:30:13 +0100

    changing colors to tertiary colors

--- admin/src/components/Chart/Chart.tsx
+++ admin/src/components/Chart/Chart.tsx
@@ -59,30 +59,19 @@ const Chart = () => {
       <h1 className="chart-title">Last 6 months revenue</h1>
       <div className="chart-container">
         <ResponsiveContainer width="100%" height="100%">
-          <AreaChart
-            width={500}
-            height={400}
-            data={data}
-            // margin={{
-            //   top: 10,
-            //   right: 30,
-            //   left: 0,
-            //   bottom: 0,
-            // }}
-          >
+          <AreaChart width={500} height={400} data={data}>
             <defs>
               <linearGradient id="total" x1="0" y1="0" x2="0" y2="1">
-                <stop offset="5%" stopColor="#69808f" stopOpacity={0.8} />
-                <stop offset="95%" stopColor="#69808f" stopOpacity={0} />
+                <stop offset="5%" stopColor="#7451f8" stopOpacity={0.8} />
+                <stop offset="95%" stopColor="#7451f8" stopOpacity={0} />
               </linearGradient>
             </defs>
             <CartesianGrid strokeDasharray="3 3" />
-            {/* <XAxis dataKey="name" /> */}
             <Tooltip />
             <Area
               type="monotone"
               dataKey="uv"
-              stroke="#69808f"
+              stroke="#7451f8"
               fillOpacity={1}
               fill="url(#total)"
             />
