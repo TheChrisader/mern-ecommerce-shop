4091d1caff8564975e160593b381bf26db2d5390
Christopher <Chris1Bobsy@Gmail.com>
2022-09-01 01:18:35 +0100

    implement login functionality

--- client/src/pages/Login/Login.tsx
+++ client/src/pages/Login/Login.tsx
@@ -1,12 +1,44 @@
+import { useState, useEffect } from "react";
 import { Link } from "react-router-dom";
+import { useDispatch, useSelector } from "react-redux";
+import { login } from "../../redux/ApiCalls";
 
 import "./Login.scss";
-
-const handleSubmit = (e: React.FormEvent<HTMLButtonElement>) => {
-  e.preventDefault();
-};
+import { useIsMount } from "../../utils/hooks/useIsMount";
 
 const Login: React.FC = () => {
+  const [username, setUsername] = useState("");
+  const [password, setPassword] = useState("");
+  const [errorMessage, setErrorMessage] = useState<string | null>(null);
+
+  const isMount = useIsMount();
+
+  const dispatch = useDispatch();
+
+  const loginError = useSelector((state: any) => state.user.error);
+
+  useEffect(() => {
+    if (isMount) {
+      return;
+    } else {
+      console.log("first");
+      setErrorMessage(loginError);
+    }
+  }, [loginError]); //eslint-disable-line react-hooks/exhaustive-deps
+
+  const handleSubmit = async (e: React.FormEvent<HTMLButtonElement>) => {
+    e.preventDefault();
+    setErrorMessage(null);
+    if (username === "" || password === "")
+      return setErrorMessage("Please input both username and password");
+    const data = {
+      username,
+      password,
+    };
+
+    await login(dispatch, data);
+  };
+
   return (
     <>
       <h1 className="login-title">Log into your Account</h1>
@@ -17,6 +49,7 @@ const Login: React.FC = () => {
             className="login-input"
             id="username"
             placeholder=" "
+            onChange={(e) => setUsername(e.target.value)}
           />
           <span className="login-input-label">Username</span>
         </label>
@@ -26,9 +59,11 @@ const Login: React.FC = () => {
             className="login-input"
             id="password"
             placeholder=" "
+            onChange={(e) => setPassword(e.target.value)}
           />
           <span className="login-input-label">Password</span>
         </label>
+        {loginError && <span className="input-error">{errorMessage}</span>}
         <button type="submit" className="login-button" onClick={handleSubmit}>
           Log in
         </button>
@@ -37,7 +72,7 @@ const Login: React.FC = () => {
       </form>
       <Link to="/register" className="link link-padding">
         <button type="submit" className="alt-button">
-          Register
+          Don't have an account? Sign up instead.
         </button>
       </Link>
     </>
