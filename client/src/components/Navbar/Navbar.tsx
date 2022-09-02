708d79bea57681095564060ff3f8bef7bfe2d27b
Christopher <Chris1Bobsy@Gmail.com>
2022-09-02 16:37:41 +0100

    change navbar links

--- client/src/components/Navbar/Navbar.tsx
+++ client/src/components/Navbar/Navbar.tsx
@@ -1,8 +1,10 @@
 import { Link } from "react-router-dom";
+import { useDispatch, useSelector } from "react-redux";
 
 import "./Navbar.scss";
 
 const Navbar = () => {
+  const user = useSelector((state: any) => state.user.currentUser);
   return (
     <nav className="navbar">
       <div className="left">
@@ -24,20 +26,29 @@ const Navbar = () => {
           <Link to="/products" className="link navbar-icon-link">
             <i className="icon fa-solid fa-magnifying-glass"></i>
           </Link>
-          <Link to="/user/:id/saved" className="link navbar-icon-link">
+          <Link
+            to={"/user/" + user._id + "/saved"}
+            className="link navbar-icon-link"
+          >
             <i className="icon fa-solid fa-heart"></i>
           </Link>
-          <Link to="/cart" className="link navbar-icon-link">
+          <Link to={"/cart/" + user._id} className="link navbar-icon-link">
             <div className="cart">
               <div className="cart-number">9+</div>
               <i className="icon fa-solid fa-cart-shopping"></i>
             </div>
           </Link>
-          <div className="icon login">
-            <Link to="/login" className="link">
-              Login
+          {user ? (
+            <Link to={"/user/" + user._id} className="nav-user link">
+              <div className="nav-user-profile"></div>
             </Link>
-          </div>
+          ) : (
+            <div className="icon login">
+              <Link to="/login" className="link">
+                Login
+              </Link>
+            </div>
+          )}
         </div>
         <div className="right"></div>
       </div>
