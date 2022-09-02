114e739b7c3b09b6b4f45825fedb7a015e2432ed
Christopher <Chris1Bobsy@Gmail.com>
2022-09-02 15:55:50 +0100

    implement products filter functionality

--- client/src/components/Products/Products.tsx
+++ client/src/components/Products/Products.tsx
@@ -5,8 +5,17 @@ import Product from "../Product/Product";
 
 import "./Products.scss";
 
-const Products: React.FC = () => {
+type filterProps = {
+  category?: string;
+  filters?: {
+    [name: string]: string;
+  };
+  sort?: string;
+};
+
+const Products: React.FC<filterProps> = ({ category, filters, sort }) => {
   const [products, setProducts] = useState([] as any);
+  const [filteredProducts, setFilteredProducts] = useState([] as any);
 
   const productsRef = useRef<HTMLDivElement | null>(null);
   const isVisible = useObserver(productsRef);
@@ -17,14 +26,45 @@ const Products: React.FC = () => {
   useEffect(() => {
     const fetchProducts = async () => {
       try {
-        const response = await axios.get("/product");
+        const response = await axios.get(
+          category ? `/product?category=${category}` : `/product`
+        );
         setProducts(response.data);
       } catch (err) {
         console.log(err);
       }
     };
     fetchProducts();
-  }, []);
+  }, [category]);
+
+  useEffect(() => {
+    filters &&
+      setFilteredProducts(
+        products.filter((item: any) =>
+          Object.entries(filters).every(([key, value]) =>
+            item[key].includes(value)
+          )
+        )
+      );
+  }, [products, filters]);
+
+  useEffect(() => {
+    switch (sort) {
+      case "newest":
+        setProducts((prev: any) =>
+          [...prev].sort((a, b) => a.createdAt - b.createdAt)
+        );
+        break;
+      case "lowest":
+        setProducts((prev: any) => [...prev].sort((a, b) => a.price - b.price));
+        break;
+      case "highest":
+        setProducts((prev: any) => [...prev].sort((a, b) => b.price - a.price));
+        break;
+      default:
+        break;
+    }
+  }, [sort]);
 
   return (
     <>
@@ -40,15 +80,25 @@ const Products: React.FC = () => {
         className={"products-wrapper " + (isVisible && "products-active")}
         ref={productsRef}
       >
-        {products.map((product: any, i: number) => (
-          <Product
-            key={i}
-            img={product?.mainImage}
-            title={product.name}
-            price={product.price}
-            slug={product.slug}
-          />
-        ))}
+        {filters
+          ? filteredProducts?.map((product: any, i: number) => (
+              <Product
+                key={i}
+                img={product?.mainImage}
+                title={product.name}
+                price={product.price}
+                slug={product.slug}
+              />
+            ))
+          : products.map((product: any, i: number) => (
+              <Product
+                key={i}
+                img={product?.mainImage}
+                title={product.name}
+                price={product.price}
+                slug={product.slug}
+              />
+            ))}
       </div>
     </>
   );
--- client/src/pages/Products/ProductsPage.tsx
+++ client/src/pages/Products/ProductsPage.tsx
@@ -1,47 +1,78 @@
+import React, { useState } from "react";
+import { useLocation, Link } from "react-router-dom";
+
 import Products from "../../components/Products/Products";
 
 import "./ProductPage.scss";
 
 const ProductsPage: React.FC = () => {
+  const location = useLocation();
+  const category = location.pathname.split("/")[2];
+
+  const [filters, setFilters] = useState<
+    undefined | { [name: string]: string }
+  >(undefined);
+  const [sort, setSort] = useState("");
+
+  const handleFilters = (e: React.ChangeEvent<HTMLSelectElement>) => {
+    let value = e.target.value;
+    setFilters((prev: any) => ({ ...prev, [e.target.name]: value }));
+  };
   return (
     <>
       <div className="filter-wrapper">
         <div className="filter-products-wrapper">
           <span className="filter-products">Filter Products:</span>
           <div className="filter-products-options-wrapper">
-            <select name="" id="" className="filter-products-options">
-              <option value="" defaultValue="" disabled>
+            <select
+              name="colors"
+              id=""
+              className="filter-products-options"
+              onChange={handleFilters}
+            >
+              <option value={undefined} defaultValue="" disabled>
                 Color
               </option>
-              <option value="">White</option>
-              <option value="">Black</option>
-              <option value="">Blue</option>
-              <option value="">Red</option>
-              <option value="">Yellow</option>
+              <option value="White">White</option>
+              <option value="Black">Black</option>
+              <option value="Blue">Blue</option>
+              <option value="Red">Red</option>
+              <option value="Gray">Gray</option>
             </select>
-            <select name="" id="" className="filter-products-options">
-              <option value="">White</option>
-              <option value="">Black</option>
-              <option value="">Blue</option>
-              <option value="">Red</option>
-              <option value="">Yellow</option>
+            <select
+              name="sizes"
+              id=""
+              className="filter-products-options"
+              onChange={handleFilters}
+            >
+              <option value={undefined} defaultValue="" disabled>
+                Sizes
+              </option>
+              <option value="S">Small</option>
+              <option value="M">Medium</option>
+              <option value="L">Large</option>
+              <option value="XL">Extra Large</option>
+              <option value="XXL">XX Large</option>
             </select>
           </div>
         </div>
         <div className="filter-products-wrapper">
           <span className="filter-products">Sort Products:</span>
           <div className="filter-products-options-wrapper">
-            <select name="" id="" className="filter-products-options">
-              <option value="">White</option>
-              <option value="">Black</option>
-              <option value="">Blue</option>
-              <option value="">Red</option>
-              <option value="">Yellow</option>
+            <select
+              name=""
+              id=""
+              className="filter-products-options"
+              onChange={(e) => setSort(e.target.value)}
+            >
+              <option value="newest">Newest</option>
+              <option value="highest">Price: highest</option>
+              <option value="lowest">Price: lowest</option>
             </select>
           </div>
         </div>
       </div>
-      <Products />
+      <Products category={category} filters={filters} sort={sort} />
     </>
   );
 };
