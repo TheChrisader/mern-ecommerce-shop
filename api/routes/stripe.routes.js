fc36277bbc5cdc7cfe2898f48442ae4e0ef887eb
Christopher <Chris1Bobsy@Gmail.com>
2022-09-08 23:48:49 +0100

    fix stripe route

--- api/routes/stripe.routes.js
+++ api/routes/stripe.routes.js
@@ -3,6 +3,6 @@ const router = require("express").Router();
 const createPayment = require("../controllers/Stripe.controller");
 const { verifyUser } = require("../utils/verifyToken");
 
-router.post("/", verifyUser, createPayment);
+router.post("/:id", verifyUser, createPayment);
 
 module.exports = router;
