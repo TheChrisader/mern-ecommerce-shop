6b717f31578b8aad9b409de52e39e90d8f191f09
Christopher <Chris1Bobsy@Gmail.com>
2022-10-25 14:30:10 +0100

    fix bug making sidebar vanish when url changes

--- admin/src/components/Sidebar/Sidebar.tsx
+++ admin/src/components/Sidebar/Sidebar.tsx
@@ -21,7 +21,9 @@ const Sidebar: React.FC<ISidebar> = ({
   const location = useLocation();
 
   useEffect(() => {
-    setState(true);
+    if (shouldSidebarShow && !state) {
+      setState(true);
+    }
   }, [location]);
 
   return (
